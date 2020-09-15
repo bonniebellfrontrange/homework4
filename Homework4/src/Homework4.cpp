cf09818f3bf8b5fc50d9bda6dd7c05022999b916
Bonnie Bell <cj6bonnie@gmail.com>
2020-09-14 23:34:40 -0600

    program stub and files

---
+++ Homework4/src/Homework4.cpp
@@ -0,0 +1,23 @@
+/*
+ * Homework4.cpp
+ *
+ *  Created on: Sep 13, 2020
+ *      Author: cj6bo
+ */
+
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+
+
+//	if ( found ) {
+//		cout <<"\nEntire substring found!" << std::endl;
+//	}
+//	else {
+//		cout <<"\nRan out of words in master file" <<std::endl;
+//	}
+}
